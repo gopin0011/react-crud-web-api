4259ade05f4a72500b347395e248876514f1199c
gopin0011 <gopin.ipin@gmail.com>
2021-12-19 17:25:38 +0700

    first commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://localhost:8000/api",
   headers: {
-    "Content-type": "application/json"
-  }
+    // "Access-Control-Allow-Origin":"*",
+    // "Access-Control-Allow-Headers":"origin, x-requested-with, content-type",
+    // "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    'Accept': 'application/json', 
+    'Content-type': 'application/json'
+  },
+  withCredentials: false
 });
